a2eb3a219bd6a58d4b867261197a2c0205b11394
Gabriel Roldan <roldan.gabriel.sergio@gmail.com>
2017-12-06 20:47:15 -0300

    Initial commit

---
+++ app/src/main/java/example/com/intentomqtt/MainActivity.java
@@ -0,0 +1,58 @@
+package example.com.intentomqtt;
+
+import android.hardware.camera2.CameraManager;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class MainActivity extends AppCompatActivity {
+
+    MQTTHerper mqttHelper;
+    TextView dataReceived;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dataReceived = findViewById(R.id.dataReceived);
+
+        startMqtt();
+    }
+
+    private void startMqtt(){
+        mqttHelper = new MQTTHerper(getApplicationContext());
+        mqttHelper.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean b, String s) {
+
+            }
+
+            @Override
+            public void connectionLost(Throwable throwable) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                Log.w("Debug",mqttMessage.toString());
+
+                dataReceived.setText(mqttMessage.toString());
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+            }
+        });
+    }
+}
