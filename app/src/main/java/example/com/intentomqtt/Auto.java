2ac1071bf04c7ea64ceb9ef8da5982b5a6913517
nanocrax <roldan.gabriel.sergio@gmail.com>
2018-08-26 20:55:32 -0300

    Mqtt+Servicio+Layoutcongif+Mensajes luego de 10 segundos

---
+++ app/src/main/java/example/com/intentomqtt/Auto.java
@@ -0,0 +1,22 @@
+package example.com.intentomqtt;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class Auto extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Lanzar Servicio
+
+        //Intent serviceIntent = new Intent();
+       // serviceIntent.setAction("exemple.com.intentomqtt.ServiceMQTT");
+        //context.startService(serviceIntent);
+        Intent i = new Intent(context, MainActivity.class);
+        // Cuando se intenta abrir una Activity desde un componente que no sea una actividad
+        // se debe de avisar mediante un Flag que está siendo iniciada por un componente alternativo.
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(i);
+    }
+}
